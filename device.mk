3cf3ad0f5d9f9c9f527fa9f4ae47c516ea85e497
itsnouralawad <itsnouralawad@gmail.com>
2024-08-14 20:08:05 +0300

    sapphire: Nuke product characteristics
    Change-Id: I66209d9628c0adf42e5b98b30eb9db2edb2e3e05
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -148,9 +148,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
-# Characteristics
-PRODUCT_CHARACTERISTICS := nosdcard
-
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
